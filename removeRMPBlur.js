8bedab561a4bca0f64167e3219922576fe2538ec
Rushil Patel <rushilpatel2806@hotmail.com>
2024-02-08 12:11:07 -0500

    Update removeRMPBlur.js

--- removeRMPBlur.js
+++ removeRMPBlur.js
@@ -1,24 +1,23 @@
 // ==UserScript==
 // @name         Remove RMP blur
-// @namespace    http://tampermonkey.net/
-// @version      2024-02-06
+// @namespace    http://realmoney.thestreet.com/
+// @version      2024-02-07
 // @description  try to take over the world!
 // @author       You
-// @match        https://*/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @match        https://realmoney.thestreet.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+    console.log("ran");
+    window.setTimeout(removeBlur, 1000);
 
-    setTimeout(init, 1000);
-
-    function init(){
-        console.log("Ran")
-        var divs = document.querySelectorAll(".bcBlur")
-        for(var i = 0; i < divs.length; i++){
-            divs[i].classList.remove('bcBlur');
+    function removeBlur(){
+        var divs = document.querySelectorAll(".bcBlur");
+        for(var i=0; i < divs.length; i++){
+            divs[i].classList.remove("bcBlur");
         }
     }
 })();
