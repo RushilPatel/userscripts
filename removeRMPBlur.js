e923ba30fe07d4459093b3b76eceedf4f3f02f48
Rushil Patel <rushilpatel2806@hotmail.com>
2024-02-08 10:01:08 -0500

    Create removeRMPBlur.js

---
+++ removeRMPBlur.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Remove RMP blur
+// @namespace    http://tampermonkey.net/
+// @version      2024-02-06
+// @description  try to take over the world!
+// @author       You
+// @match        https://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setTimeout(init, 2000);
+
+    function init(){
+        console.log("Ran")
+        var divs = document.querySelectorAll(".bcBlur")
+        for(var i = 0; i < divs.length; i++){
+            divs[i].classList.remove('bcBlur');
+        }
+    }
+})();
